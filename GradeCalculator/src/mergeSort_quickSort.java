3a723219e1c0f1ab0c916985be07b6c86601bb56
Brandon Phan <Brandon.phan.73@gmail.com>
2016-06-06 13:52:14 -0700

    Create mergeSort_quickSort.java
    Initial Commit

---
+++ GradeCalculator/src/mergeSort_quickSort.java
@@ -0,0 +1,13 @@
+
+public class MergeSort_QuickSort {
+
+  public int[] array = {2,6,12,11,86,113,5,25,34,7};
+  
+  public static void main(String[] args) {
+    
+    for(int i = 0; i <= array.length; i++) {
+      system.out.println(i);
+    }
+  }
+
+}

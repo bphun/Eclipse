1de69b7f5d83c348a506a90eaf7260eb0ca5abb2
Brandon Phan <Brandon.phan.73@gmail.com>
2016-08-17 19:52:40 -0700

    Work in progress

--- MakeADiamond.java
+++ MakeADiamond.java
@@ -16,23 +16,49 @@ public class MakeADiamond  implements Directions{
 
 	private void drawDiamond() {
 		Robot robot = new Robot(1,5,North,30);
-		int beeperCount = 20;
-                int currentBeeper = 0;
-
-                while (currentBeeper < 4) {
-                      robot.putBeeper();
-                      robot.move();
-                      robot.turnLeft();
-                      robot.move();
-                      robot.putBeeper();
-                      if (currentBeeper != 4) {
- 
-                      }
-
-                }
+	   int beeperCount = 20;
+      int currentBeeper = 0;
+      
+      drawBottomLeft(robot);
+          
+      drawTopLeft(robot);
 	} 
-
 	
+	void drawBottomLeft(Robot robot) {
+	    while (currentBeeper < beeperCount / 4 - 1) {
+                robot.putBeeper();
+                robot.move();
+                robot.turnLeft();
+                robot.move();
+                robot.putBeeper();
+                if (currentBeeper != 4) {
+                     turnRight(robot);
+                }
+        }
+          currentBeeper = null;
+	}
+	
+  void drawTopLeft(Robot robot) {
+      while (currentBeeper < beeperCount / 4 - 1) {
+                 robot.move();        // Might have to change to 'turnRight(robot)'
+                 turnRight(robot);
+                 robot.move();
+                 robot.putBeeper();
+                 robot.turnLeft();
+                 //wrobot.move();
+                 //turnRight(robot);
+                 //robot.move();
+                 //robot.putBeeper();                       
+                 currentBeeper++;                                                      
+      }
+      currentBeeper = null;
+  }
 
-
+  void turnRight(Robot robot) {
+      robot.turnLeft();
+      robot.turnLeft();
+      robot.turnLeft();
+      
+   }
+           
 }

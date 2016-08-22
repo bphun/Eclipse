e65d1338a28c8007867d127bcb37fd9e842e42e4
Brandon <Brandon.phan.73@gmail.com>
2016-08-22 11:52:33 -0700

    8/22/16

--- DiamondDrawing/src/MakeACircle.java
+++ DiamondDrawing/src/MakeACircle.java
@@ -10,7 +10,7 @@ public class MakeACircle implements Directions {
 		int circleDiameter = Integer.parseInt(circleDiameterStr);
 		
 		World.setVisible(true);
-		World.setDelay(20);
+		World.setDelay(0);
 		
 		circle.drawCircle(circleDiameter);
 		
@@ -19,25 +19,12 @@ public class MakeACircle implements Directions {
 	private void drawCircle(int diameter) {
 		Robot robot;
 		int circleRadius = diameter / 2;
+		double circleCircumfrence = 2 * Math.PI * circleRadius;
 		
 		World.setSize(diameter, diameter);
-		robot = new Robot(1,circleRadius, North, infinity);
-		
-		robot.putBeeper();
-		robot.move();
-		turnRight(robot);
-		robot.move();
-		robot.putBeeper();
-		robot.move();
-		robot.turnLeft();
-		robot.move();
-		robot.putBeeper();
-		robot.move();
-		robot.turnLeft();
-		robot.move();
-		robot.putBeeper();
-		robot.move();
-		robot.putBeeper();
+		robot = new Robot(1,circleRadius, South, infinity);
+
+		goToCenter(robot, circleRadius);
 
 		
 	}
@@ -48,4 +35,22 @@ public class MakeACircle implements Directions {
 		robot.turnLeft();
 	}
 	
+	private void turnAround(Robot robot) {
+		robot.turnLeft();
+		robot.turnLeft();
+	}
+	
+	private void goToCenter(Robot robot, int circleRadius) {
+		
+		Direction robotDirection = robot.direction();
+		
+		if (robotDirection != North) {
+			turnAround(robot);
+		}
+		
+		
+		for (int i = 0; i < circleRadius; i++) {
+			robot.move();
+		}
+	}
 }

283adfe964d058205abc722bc44f26de840b0437
Brandon <Brandon.phan.73@gmail.com>
2016-08-23 21:03:07 -0700

    Finished MakeACircle.java, added comments to MakeADiamond.java

--- DiamondDrawing/src/MakeACircle.java
+++ DiamondDrawing/src/MakeACircle.java
@@ -3,90 +3,107 @@ import kareltherobot.*;
 
 public class MakeACircle implements Directions {
 		
+	static int circleDiameter;
 	int slideCount;
 	
 	public static void main(String[] args) {
 		MakeACircle circle = new MakeACircle();
 		
+		//	Prompt the user for their desired circle diameter then convert that value from a 'String' to an 'int'
 		String circleDiameterStr = JOptionPane.showInputDialog(null, "What diameter circle would you like");
-		int circleDiameter = Integer.parseInt(circleDiameterStr);
+		circleDiameter = Integer.parseInt(circleDiameterStr);
 		
+		//	Make the world visible, and set the delay to '0' so that Karel draws the circle quickly
 		World.setVisible(true);
-		World.setDelay(10);
+		World.setDelay(5);
 		
+		//	Draw the circle
 		circle.drawCircle(circleDiameter);	
 	}
 	
 	private void drawCircle(int diameter) {
 		Robot robot;
+		
 		int circleRadius = diameter / 2;
-		int circleCenterX = circleRadius;
-		int circleCenterY = circleRadius;
 		
-		World.setSize(diameter + 2, diameter + 2);
-		robot = new Robot(circleRadius + 2,circleRadius + 2, North, infinity);
-		robot.putBeeper();	//	Place a beeper to mark the center point
+		//	Set the worlds size to 'diameters + 1' so that we can fit the circle into the world
+		World.setSize(diameter + 1, diameter + 1);
 		
-		for (int quadrants = 0; quadrants <= 3; quadrants++) {
-			for (int i = 0; i < circleRadius; i ++) {
-				robot.move();
-			}
-			robot.putBeeper();
-			goToCenter(robot, circleRadius);
-			robot.turnLeft();		
-		}	
-		generateBeeperCoordinate(circleRadius, robot);
+		//	Add Karel to the center of the view, facing North, and turn off the beeper limit
+		robot = new Robot(circleRadius + 1,circleRadius + 1, North, infinity);
 		
-	}
-	
-	private beeper[] generateBeeperCoordinate(int circleRadius, Robot robot) {
-		int circleCenterX = circleRadius;
-		int circleCenterY = circleRadius;
-		
-		int currentXPos = robot.street();
-		int currentYPos = robot.avenue();
-		
-		int distanceToCenter = getDistanceFromCenter(currentXPos, currentYPos, circleCenterX, circleCenterY);	
-		int circleCircumfrence = (int) (2 * Math.PI * circleRadius);
-		
-		int currentCord = 0;
-		
-		beeper beeperCord;
-		beeper[] beeperCoordinateArray = new beeper[0];
+		//	Put a beeper at the center of the world so that we can mark the circle's center point
+		robot.putBeeper();
 				
-		while (beeperCoordinateArray.length < circleCircumfrence / 4) {
-			
-			for (int quadrants = 0; quadrants <= 3; quadrants++) {
-				
-				slideLeft(robot);
-				for (int steps = 0; steps < circleRadius; steps ++) {
-					int distance = (int) (Math.sqrt((circleRadius * circleRadius - steps*steps)));
-					distance = (int) Math.round(distance);
-					
-					move(distance, robot);
-				}
-				robot.putBeeper();
-				goToCenter(robot, circleRadius);
-				robot.turnLeft();		
-			}
-			
+		//	Slide Karel all of the way to the right then slide him to the bottom of the world, we'll use this as our starting point
+		for (int slides = 0; slides < circleRadius; slides++) {
+			slideRight(robot);
 		}
-		return beeperCoordinateArray;
+		turnAround(robot);		
+		for (int i = 0; i < circleRadius; i++) {
+			robot.move();
+		}
+		turnAround(robot);
+		
+		//	MARK:	Begin drawing the circle
+		
+		//	Create a loop that iterates through all of the x values on the coordinate plane
+		for (int loopTimes = circleDiameter; loopTimes > 0; loopTimes--) {
+			
+			//	Move Karel to the top of the world, checking our distance from the circle's center point every move
+			for(int stepsUp = 0; stepsUp < circleRadius * 2; stepsUp++) {
+				robot.move();
+				checkDistanceFromCenter(robot, circleRadius);
+			}
+			
+			//	Set up Karel to begin the next row
+			slideLeft(robot);
+			checkDistanceFromCenter(robot, circleRadius);
+			turnAround(robot);
+			
+			//	Move karel to the bottom of the world, checking our distance from the circle's center point every move
+			for (int stepsDown = 0; stepsDown < circleRadius * 2; stepsDown++) {
+				robot.move();
+				checkDistanceFromCenter(robot, circleRadius);
+			}
+			
+			//	Set up Karel to begin the next row
+			slideRight(robot);
+			checkDistanceFromCenter(robot, circleRadius);
+			turnAround(robot);	
+		}	
 	}
 	
+	//	Method used to check Karel's distance from the circle's center point
 	private int getDistanceFromCenter(int currentXPos, int currentYPos, int centerXCord, int centerYCord) {
+		//	Note: Distance Formula: d = √(x2 - x1)^2 + (y2 - y1)^2
+		
+		int distanceFromCenter;
 		int xDif = currentXPos - centerXCord;
 		int yDif = currentYPos - centerYCord;
 		
-		int distanceFromCenter = (xDif * xDif) + (yDif * yDif);
-		distanceFromCenter = (int) (Math.sqrt(distanceFromCenter));
-		distanceFromCenter = Math.round(distanceFromCenter);
-		
-		System.out.print("Distance from center: " + distanceFromCenter + "\n");
-		
+		distanceFromCenter = (int) Math.sqrt((xDif * xDif) + (yDif * yDif));
+				
 		return distanceFromCenter;
 	}
+	
+	//	Method used to check if Karel's distance from the circle's center point is equal to the circle's desired radius, if it is then we will put down a beeper	
+	private void checkDistanceFromCenter(Robot robot, int circleRadius) {
+		int currentXPos = robot.avenue() - 1;
+		int currentYPos = robot.street() - 1;
 		
+		int circleCenterX = circleRadius;
+		int circleCenterY = circleRadius;
+				
+		int distanceFromCenter = getDistanceFromCenter(currentXPos, currentYPos, circleCenterX, circleCenterY);
+				
+		if (distanceFromCenter == circleRadius) {
+			robot.putBeeper();
+		}
+	}
+	
+	//	MARK:	Karel movement helper methods
+	
 	private void turnRight(Robot robot) {
 		robot.turnLeft();
 		robot.turnLeft();
@@ -98,13 +115,6 @@ public class MakeACircle implements Directions {
 		robot.turnLeft();
 	}
 	
-	private void goToCenter(Robot robot, int circleRadius) {	
-		turnAround(robot);
-		for (int i = 0; i < circleRadius; i++) {
-			robot.move();
-		}
-	}
-	
 	private void slideLeft(Robot robot) {
 		robot.turnLeft();
 		robot.move();
@@ -114,29 +124,10 @@ public class MakeACircle implements Directions {
 	}
 	
 	private void slideRight(Robot robot) {
-
-		
 		turnRight(robot);
 		robot.move();
 		robot.turnLeft();
 		slideCount++;
 	}
-
-	private void move(int n, Robot robot) {
-		while (n > 0) {
-			robot.move();
-			n--;
-		}
-	}
 }
 
-class beeper {
-	
-	int xCord;
-	int yCord;
-	
-	beeper(int beeperXCord, int beeperYCord) {
-		beeperXCord = xCord;
-		beeperYCord = yCord;
-	}
-}
--- DiamondDrawing/src/MakeADiamond.java
+++ DiamondDrawing/src/MakeADiamond.java
@@ -7,31 +7,44 @@ public class MakeADiamond  implements Directions {
 	public static void main(String[] args) { 
 		MakeADiamond diamond = new MakeADiamond(); 
 
+		//	Prompt the user to input their desired diamond side length then convert it from a 'String' to an 'int'
 		String sideLengthPrompt = JOptionPane.showInputDialog(null, "How large of a side do you want?");
 		int sideLength = Integer.parseInt(sideLengthPrompt);	
 
+		//	Set the delay to 0 so that Karel draws faster, display the world
 		World.setDelay(0);
 		World.setVisible(true);
 				
+		//	Draw the diamond
 		diamond.drawDiamond(sideLength);
 	}
+	
+	//	MARK: Diamond drawing method
 
 	private void drawDiamond(int sideLength) {
 		Robot robot;
 		
+		//	Set the scale the world so that it fits the diamond
 		World.setSize(sideLength * 2, sideLength *  2);
+		
+		//	Initialize the robot at the bottom of the y-axis and center of the x-axis, facing north, remove the beeper count limit
 		robot = new Robot(1, sideLength, North, infinity);
 
 		final int beeperCount = sideLength * 4;
 		System.out.print("Total beepers to Draw: " + beeperCount);
 		
+		//	Declare 'sideCount' variable to keep track of the current side being drawed, use 'currentBeeper' variable to keep track of what beeper is currently being drawed
 		int sideCount = 0;
 		int currentBeeper;
+		
+		//	Draw the desired # of beepers on all 4 sides
 		while (sideCount <= 3) {
 			
 			System.out.println("Currently drawing Side: " + sideCount);
 
+			//	Reset the current beeper count so that we can draw the first beeper of the diamond's side
 			currentBeeper = 0;
+			
 			while (currentBeeper < (sideLength - 1)) {			
 				int remainingBeepers = beeperCount - currentBeeper;
 				System.out.println("Remaining Beepers: " + remainingBeepers);
@@ -48,6 +61,8 @@ public class MakeADiamond  implements Directions {
 			sideCount++;
 		}
 	}	
+	
+	//	MARK: Karel helper methods
 	
 	void turnRight(Robot robot) {
 		robot.turnLeft();

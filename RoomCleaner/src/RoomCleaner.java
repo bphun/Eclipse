b1ab4724ab3e8b2a82be3a72e123fddac0e8b4c2
Brandon <Brandon.phan.73@gmail.com>
2016-08-30 12:51:42 -0700

    8/30/16; Finished, prints out all needed info

--- RoomCleaner/src/RoomCleaner.java
+++ RoomCleaner/src/RoomCleaner.java
@@ -26,20 +26,35 @@ public class RoomCleaner implements Directions {
 		// file.  It then opens the world file and creates the robot
 
 		String wrldName = "basicRoom.wld";
+//		String wrldName = JOptionPane.showInputDialog(null, "What is the name of the world file you want to load?");
 		World.setVisible(true);	
 		World.setDelay(0);
 		World.readWorld(wrldName);		
 	}
 
 	private void cleanRoom() {
-		robot = new Robot(7,7, East, infinity);
-		//		String robotXPositionStr = JOptionPane.showInputDialog(null, "What X coordinate do you want to place the robot on");
-		//		int robotXPosition = Integer.parseInt(robotXPositionStr);
-		//
-		//		String robotYPositionStr = JOptionPane.showInputDialog(null, "What Y coordinate do you want to place the robot on");
-		//		int robotYPosition = Integer.parseInt(robotYPositionStr);
-		//
-		//		robot = new Robot(robotYPosition, robotXPosition, North, infinity);
+//		robot = new Robot(7,7, East, infinity);
+		
+		String robotXPositionStr = JOptionPane.showInputDialog(null, "What X coordinate do you want to place the robot on");
+		int robotXPosition = Integer.parseInt(robotXPositionStr);
+
+		String robotYPositionStr = JOptionPane.showInputDialog(null, "What Y coordinate do you want to place the robot on");
+		int robotYPosition = Integer.parseInt(robotYPositionStr);
+
+		String robotStartingDirection = JOptionPane.showInputDialog("What direction do you want karel to face?" + "\n" + "North" + "\n" + "East" + "\n" + "South" + "\n" + "West");
+		Direction startingDirection = null;
+		
+		if (robotStartingDirection.equalsIgnoreCase("north")) {
+			startingDirection = North;
+		} else if (robotStartingDirection.equalsIgnoreCase("east")) {
+			startingDirection = East;
+		} else if (robotStartingDirection.equalsIgnoreCase("south")) {
+			startingDirection = South;
+		} else if (robotStartingDirection.equalsIgnoreCase("west")) {
+			startingDirection = West;
+		}
+		
+		robot = new Robot(robotYPosition, robotXPosition, startingDirection, infinity);
 
 		int roomArea = getRoomArea();
 		System.out.print("Room Area: " + roomArea + "\n");
@@ -66,75 +81,86 @@ public class RoomCleaner implements Directions {
 				break;
 			}
 		}	
-
-		goToBottomLeftCorner();
-		faceEast();
-		while (true) {
-			if (robot.frontIsClear()) {
-				robot.move();
-				sortBeeperArray();
-			} else if ((!robot.frontIsClear()) && (robot.facingEast())) {
-				if (leftIsClear()) {
-					slideLeft();
-					turnAround();
-					sortBeeperArray();
-				} else {
-					break;
-				}
-			} else if ((!robot.frontIsClear()) && (robot.facingWest())) {
-				slideRight();
-				turnAround();
-				sortBeeperArray();
-			} else if ((!robot.frontIsClear() && (robot.facingNorth()))) {
-				robot.turnLeft();
-			} else if ((robot.facingNorth()) && (!rightIsClear())) {
-				break;
-			}
-		}	
-
-		for (Beeper b:beeperArray) {
-			System.out.println(b);
-		}
-		System.out.print("Beeper array size: " + beeperArray.size() + "\n");
 	}
-
 	private void printResults() {
 		// A bunch of System.out.prints go here
+		int largestBeeperPileSize = getLargestBeeperPileSize();
+		double averageBeeperPileSize = calculateAverageBeeperPileSize();
+		double percentDirty = getPercentDirty();
+		ArrayList<Integer> distanceFromLargestBeeperPile = getDistanceFromLargestBeeperPile();
+		
 		System.out.print("The area of the room is: " + roomArea +"\n");
-		System.out.print("The total number of beeper piles picked up was: " + beeperArray.size() + "\n");	// Should be 9
 		System.out.print("Karel picked up a total of " + totalBeepers + " beepers" + "\n");
+		System.out.print("The total number of beeper piles picked up was: " + beeperArray.size() + "\n");
+		System.out.print("The largest beeper pile was: " + largestBeeperPileSize + "\n");
+		System.out.print("The X coordinate of the largest beeper pile was: " + distanceFromLargestBeeperPile.get(0) + "\n" + "The Y coordiante of the largest beeper pile was: " + distanceFromLargestBeeperPile.get(1) + "\n");
+		System.out.print("The average beeper pile size is: " + averageBeeperPileSize + "\n");
+		System.out.print("The percent dirty is: " + percentDirty + "%" + "\n");
 	}
+	private int getLargestBeeperPileSize() {
+	    int largestPile = beeperArray.get(0).beeperPileSize;
 
-	private void sortBeeperArray() {
+	    for(int i=0; i < beeperArray.size(); i++){
+	        if(beeperArray.get(i).beeperPileSize > largestPile){
+	        	largestPile = beeperArray.get(i).beeperPileSize;
+	        }
 
-		for (int i = 0; i < beeperArray.size(); i++) {
-			int currentXPos = robot.avenue();
-			int currentYPos = robot.street();
+	    }
+	    return (largestPile);
+	}	
+	private ArrayList<Integer> getDistanceFromLargestBeeperPile() {
+	    Beeper largestPile = beeperArray.get(0);
 
-			if ((currentXPos == beeperArray.get(i).beeperXCord) && (currentYPos == beeperArray.get(i).beeperYCord)) {
-				beeperArray.get(i).beeperPileSize++;
+	    for(int i=0; i < beeperArray.size(); i++){
+	        if(beeperArray.get(i).beeperPileSize > largestPile.beeperPileSize){
+	        	largestPile = beeperArray.get(i);
+	        }
+
+	    }
+	    ArrayList<Integer> largestPileCoordinate = new ArrayList<Integer>();
+	    
+	    largestPileCoordinate.add(largestPile.beeperXCord);
+	    largestPileCoordinate.add(largestPile.beeperYCord);
+	    
+	    return largestPileCoordinate;
+	}
+	private double calculateAverageBeeperPileSize() {
+		Integer sum = 0;
+		if(!beeperArray.isEmpty()) {
+			for (int i = 0; i < beeperArray.size(); i++) {
+				sum += beeperArray.get(i).beeperPileSize;
 			}
+			return sum.doubleValue() / beeperArray.size();
 		}
-
+		return sum;
 	}
-
+	private double getPercentDirty() {		
+		int beeperArraySize = beeperArray.size();
+		double percentDirty = ((double)beeperArraySize / roomArea) * 10;
+		return percentDirty;
+	}
 	private void pickBeeper() {
 
 		int beeperXPos = 0;
 		int beeperYPos = 0;
+		int beeperPileSize = 0;
 
 		Beeper beeper;
 
 		while (robot.nextToABeeper()) {
-			int beeperPileSize = 0;
 			beeperXPos = robot.avenue();
 			beeperYPos = robot.street();
 
 			robot.pickBeeper();
-			beeperPileSize = 1;
+			beeperPileSize++;
 			totalBeepers++;
-			beeper = new Beeper(beeperPileSize, beeperXPos, beeperYPos);	
+		}
+		
+		if (beeperPileSize != 0) {
+			beeper = new Beeper(beeperPileSize, beeperXPos, beeperYPos);
+			System.out.println("Picked up a pile of " + beeperPileSize + " beepers");
 			beeperArray.add(beeper);
+			beeperPileSize = 0;	
 		}
 	}
 	private int getRoomArea() {

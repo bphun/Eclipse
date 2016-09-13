534d4def79b1fbe54b66abb0c32fb4c659a1e8f6
Brandon <Brandon.phan.73@gmail.com>
2016-09-13 12:29:20 -0700

    Fixed bug, won't display greeting when it is 12 o'clock

--- MediaPlayer/MediaLib/app/src/main/java/org/example/pltw/medialib/Greeting.java
+++ MediaPlayer/MediaLib/app/src/main/java/org/example/pltw/medialib/Greeting.java
@@ -19,12 +19,11 @@ public class Greeting {
         String greeting = "";
         if (hour < 12) {
             greeting = "Good Morning... \n Welcome to your media library";
-        } else if ((hour > 12) && (hour < 16)) {
+        } else if ((hour >= 12) && (hour <= 16)) {
             greeting = "Good Afternoon... \n Welcome to your media library";
         } else if (hour >= 16) {
             greeting = "Good Evening... \n Welcome to your media library";
         }
-
         return greeting;
     }
 }

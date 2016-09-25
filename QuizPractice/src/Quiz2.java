e06c5165c8297a4382b443fd39ccc0a13f9e0e70
Brandon <Brandon.phan.73@gmail.com>
2016-09-25 13:45:57 -0700

    Added numVowels method

--- QuizPractice/src/Quiz2.java
+++ QuizPractice/src/Quiz2.java
@@ -1,24 +1,51 @@
+import java.util.ArrayList;
 
 public class Quiz2 {
 
 	public static void main(String[] args) {
-		numOccur("hello", 'h');
+		
+		int numOccur = numOccur("hello", 'h');
+		System.out.print("Num Occur: " + numOccur + "\n");
+		
+		int numVowels = numVowels("Hello");
+		System.out.print("Num Vowels: " + numVowels + "\n");
+		
 	}
 	
-	public static int numOccur(String s, char let) {
-		
-		s = s.toLowerCase();
-		
+	private static int numOccur(String s, char let) {
 		int numOccur = 0;
-		
+
+		s = s.toLowerCase();
+
 		for (int i = 0; i < s.length(); i++) {
 			if (s.charAt(i) == let) {
 				numOccur++;
 			}
 		}
-		
-		System.out.print("Num occur: " + numOccur);
 		return (numOccur);
 	}
+	
+	private static int numVowels(String s) {
+		int numVowels = 0;
+		ArrayList<Character> vowelList = new ArrayList<Character>();
+		
+		vowelList.add('a');
+		vowelList.add('e');
+		vowelList.add('i');
+		vowelList.add('o');
+		vowelList.add('u');
+
+		s = s.toLowerCase();
+		
+		for (int character = 0; character < s.length(); character++) {
+			for (int vowel = 0; vowel < vowelList.size(); vowel++) {
+				if (s.charAt(character) == vowelList.get(vowel)) {
+					numVowels++;
+				}
+			}
+		}
+		return (numVowels);
+	}
+	
 	
 }

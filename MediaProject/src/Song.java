330ff1a56617402e5c7ac434210f9a2966dfb8ca
Brandon <Brandon.phan.73@gmail.com>
2016-10-07 12:02:56 -0700

    Added ParseSong

--- MediaProject/src/Song.java
+++ MediaProject/src/Song.java
@@ -83,4 +83,12 @@ public class Song {
 
 		return sortedSongs;
 	}
+	
+	public ArrayList<Song> scrambleSongList(ArrayList<Song> unscrambledSongs) {
+		ArrayList<Song> scrambledSongs = new ArrayList<Song>();
+		
+		
+		
+		return scrambledSongs;
+	}
 }

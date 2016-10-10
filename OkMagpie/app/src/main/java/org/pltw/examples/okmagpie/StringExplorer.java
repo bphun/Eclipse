8c440e54bfbeb8d32c19c342e9ad7508b08403bf
Brandon <Brandon.phan.73@gmail.com>
2016-10-10 16:50:01 -0700

    Finished okMagpie project

--- OkMagpie/app/src/main/java/org/pltw/examples/okmagpie/StringExplorer.java
+++ OkMagpie/app/src/main/java/org/pltw/examples/okmagpie/StringExplorer.java
@@ -24,6 +24,29 @@ public class StringExplorer
 		
 		//  Try other methods here:
 
+	private int findKeyword(String statement, String goal, int startPos) {
+		String phrase = statement.trim();
+		int psn = phrase.toLowerCase().indexOf(goal.toLowerCase(), startPos);
+		while (psn >= 0) {
+			String before = " ", after = " ";
+			if (psn > 0) {
+				before = phrase.substring (psn - 1, psn).toLowerCase();
+			}
+			if (psn + goal.length() < phrase.length()) {
+				after = phrase.substring(psn + goal.length(),
+						psn + goal.length() + 1).toLowerCase();
+			}
+			/* determine the values of psn, before, and after at this point in the method. */ if (((before.compareTo ("a") < 0 ) || (before.compareTo("z") > 0)) && ((after.compareTo ("a") < 0 ) || (after.compareTo("z") > 0))) {
+			return psn; 
+			}
+			psn = phrase.indexOf(goal.toLowerCase(), psn + 1);
+		}
+		return -1; y44
+	}
+
+}
+
+
 	}
 
 }

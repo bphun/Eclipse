c7077f0be9f60e8aef08a03dc95b87ffa55b1b6f
Brandon <Brandon.phan.73@gmail.com>
2016-10-20 17:44:47 -0700

    Updated application activities

--- MediaProject/src/LoopingMediaLib.java
+++ MediaProject/src/LoopingMediaLib.java
@@ -11,9 +11,31 @@ public class LoopingMediaLib {
 				
 				System.out.print("Songs" + "\n" + "-----------------" + "\n");
 
+				String title = "";
+				String album = "";
+				String artist = "";
+				double duration = 0.0;
+				int rating = 0;
+				
 				for (final String n : regex) {
-					System.out.println(n);
-				}			
+					if (n.contains("Title: ")) {
+						title = n.substring(7);
+					} else if (n.contains("Album: ")) {
+						album = n.substring(8);
+					} else if (n.contains("Artist: ")) {
+						artist = n.substring(9);
+					} else if (n.contains("Duration: ")) {
+						duration = Double.parseDouble(n.substring(10));
+					} else if (n.contains("Rating: ")) {
+						rating = Integer.parseInt(n.substring(9));
+					}
+//					System.out.println(n);
+				}	
+				System.out.println(title);
+				System.out.println(album);
+				System.out.println(artist);
+				System.out.println(duration);
+				System.out.println(rating);
 			} else if (songInfo == null){
 				break;
 			}

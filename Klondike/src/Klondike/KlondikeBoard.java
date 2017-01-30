f6da56d8132d6a72d50483d6f1a8c60d34cf8d3f
Brandon <Brandon.phan.73@gmail.com>
2017-01-30 11:33:48 -0800

    Commited methods

--- Klondike/src/Klondike/KlondikeBoard.java
+++ Klondike/src/Klondike/KlondikeBoard.java
@@ -14,18 +14,21 @@ public class KlondikeBoard {
 	//	A list of seven Piles that are drawn horizontally on the Board
 	private static List<Pile> piles;
 
+	//	The pile of cards that is used to get additional cards to play with
 	private static Pile deck;
+
+	//	The pile of cards that can be added to the playing piles
 	private static Pile usablePile;
 
+	//	The List of cards that holds all the aces, when full, the game is won
 	private static Card[] aceList;
 
 	private static final String[] RANKS = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
 
 	private static final String[] SUITS = {"spades", "hearts", "diamonds", "clubs"};
 
+	//	The list containing lists of cards that where selected and ready to swap between playing piles
 	private List<List<Card>> selectedCards;
-
-	private static final String BACK_CARD_FILE_NAME = "cards/back1.GIF";
 
 	public KlondikeBoard() {
 		aceList = new Card[4];
@@ -77,19 +80,32 @@ public class KlondikeBoard {
 	* or not the click was in the cards boundaries
 	*/
 	public void clickedAt(int x, int y) {
+		//	Check if the deck was pressed, if so, deal a card to the usable pile
 		if (clickedTopDeck(x, y)) {
 			addCardToUsablePile();
 			return;
 		}
 
+		//	Check if the usablePile was pressed, if so, select it and add it to the selectedCards list
 		if (clickedUsablePile(x, y)) {
 			moveCardToSelectedPlayPile(x, y);
 			return;
 		}
 
+		/**
+		 *	Check if a playing pile was pressed, if so, check if a another pile is selected, 
+		 * then move the selected card to the second pile
+		 */
 		clickedPlayingPile(x, y);
 	}
 
+	/**
+	 * @param x is the X coordinate of the click
+	 * @param y is the Y coordinate of the click
+	 * 1. Checks if an ace is clicked in any of the playing piles, if one is then it will move it to the aceList and return
+	 * 2. Checks if any card that is not an ace is clicked, selects it, checks if another card is 
+	 * selected, and then moves the first selected card to the second selected card's pile
+	 */
 	private void clickedPlayingPile(int x, int y) {
 		for (Pile p : piles) {
 			for (int i = p.size() - 1; i >= 0; i--) {
@@ -148,9 +164,41 @@ public class KlondikeBoard {
 		}
 	}
 
+	/** 
+	 * @param x is the X coordinate of the click
+	 * @param y is the Y coordinate of the click
+	 * Moves a card from the 'usablePile' to selected 'playingPile'
+	 */
 	private void moveCardToSelectedPlayPile(int x, int y) {
 		Card c = usablePile.cards().get(usablePile.size() - 1);
 
+		if (c.rank().equalsIgnoreCase("ace")) {
+			int index = 0;
+			switch (c.suit()) {
+				case "spades":
+				index = 0;
+				break;
+			case "hearts":
+				index = 1;
+				break;
+			case "diamonds":
+				index = 2;
+				break;
+			case "clubs":
+				index = 3;
+				break;
+			 }
+			aceList[index] = c;
+			c.setSelected();
+
+			removeCardsFrom(p);
+
+			if (p.get(p.size() - 1).faceDown()) {
+				p.get(p.size() - 1).flip();
+			}
+			resetCards();
+			return;
+		}
 		if (c.containsPoint(x, y) != null) {
 			List<Card> selectedCard = new ArrayList<>();
 			selectedCard.add(c);
@@ -168,20 +216,40 @@ public class KlondikeBoard {
 		}
 	}
 
+	/**
+	 * @param x is the X coordinate of the click
+	 * @param y is the y coordiante of the click
+	 * Checks if the top most card in 'usablePile' was clicked and returns true if it was, false otherwise
+	 * Done by calling the usablePile's last card's containsPoint method
+	 */
 	private boolean clickedUsablePile(int x, int y) {
-		if ((usablePile != null) && (usablePile.cards().get(usablePile.size()- 1).containsPoint(x, y) != null)) {
+		if ((usablePile != null) && (usablePile.cards().get(usablePile.size() - 1).containsPoint(x, y) != null)) {
 		 return true;
 		}
 		return false;
 	}
 
+	/**
+	 * @param x is the x coordinate of the click
+	 * @param y is the y coordinate of the click
+	 * Checks if the deck was pressed, and returns true if it was, false otherwise
+	 * Done by calling the deck's top-most card's containsPoint method
+	 */
 	private boolean clickedTopDeck(int x, int y) {	
 		if (deck.get(0).containsPoint(x, y) != null) { return true; }
 		return false;
 	}
 
+	/**
+	 * Adds a card to the 'usablePile', 
+	 * 1. checks if the 'usablePile' is empty, returns if it is
+	 * 2. Checks if the 'usablePile' is null, if it is, then we will initialize the pile with one card.
+	 * If the previous wasn't executed, we will check if the pile contains three cards(max # of cards we can have in this pile),
+	 * meaning that we must clear the pile and re-deal the cards again.
+	 * If any of the previosu weren't executed then we will just add another card to the pile
+	 */
 	private void addCardToUsablePile() {
-		if ((usablePile != null) &&(usablePile.empty())) { return; }
+		if ((usablePile != null) && (usablePile.empty())) { return; }
 
 		if (usablePile == null) {
 			usablePile = new Pile(1);
@@ -203,6 +271,10 @@ public class KlondikeBoard {
 		return;
 	}
 
+
+	/**
+	 *
+	 */
 	private void transferCards() {
 		Pile destinationPile = null;
 		Pile startPile = null;

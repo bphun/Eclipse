a25085fb41fb52b211d2a86a64e6b582f6c0a354
Brandon <Brandon.phan.73@gmail.com>
2017-02-01 12:22:34 -0800

    Can sort cards

---
+++ Klondike/src/Klondike/CardCompare.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class CardCompare implements Comparator<Card> {
+
+	@Override
+	public int compare(Card c1, Card c2) {
+		Integer c1Rank = (Integer) c1.intRank();
+		Integer c2Rank = (Integer) c2.intRank();
+
+		return c1Rank.compareTo(c2Rank);
+	}
+
+}

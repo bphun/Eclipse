cf903b1b241411b526f069595152dddd6ac9c042
Brandon <Brandon.phan.73@gmail.com>
2017-03-16 23:04:06 -0700

    Initial Commit. Stores one int[][] array. Used in the LifeAsWeKnowIt class to allow for us to rewind to previous generations

---
+++ GameOfLife/src/TwoDimensionArray.java
@@ -0,0 +1,13 @@
+public class TwoDimensionArray {
+
+	int[][] array;
+	
+	public TwoDimensionArray(int[][] array) {
+		this.array = array;
+	}
+	
+	public int[][] array() {
+		return array;
+	}
+	
+}
